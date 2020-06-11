ba7cb68efacd231198b11588cef5c0af7cabf7ee
Manne Tallmarken <mannet@kth.se>
2020-06-11 13:18:26 +0200

    add perl scripts used to parse log file, averaging values and transforming coordinates

---
+++ parse_log.pl
@@ -0,0 +1,374 @@
+#!/usr/bin/perl
+use strict;
+use JSON;
+
+$| = 1;
+
+use constant {
+    WAITING_HEADER    => 0,
+    EXPECTING_RX_NODE => 1,
+    EXPECTING_TX_NODE => 2,
+    NUM_NODES         => 10
+};
+
+my $state = WAITING_HEADER;
+my $cnt = 0;
+my $addrTo;
+my %strengths;
+my %gpsCache;
+my $dumpGps = 1;
+my $gpsFh;
+
+
+if ($dumpGps)
+{
+    open ($gpsFh, ">log.gps") or die;
+}
+
+my %lookup =
+(
+    "2bc6" => "andersson",
+    "2bc5" => "brolin",
+    "2bcb" => "dahlin",
+    "2bd0" => "hamrin",
+    "2bc8" => "larsson",
+    "2bc9" => "ljungberg",
+    "2bc7" => "mellberg",
+    "2bcf" => "ravelli",
+    "2bce" => "skoglund",
+    "2bca" => "zlatan"
+);
+sub on_sigint
+{
+    print "on_sigint";
+    print_strengths (1);
+    exit (1);
+}
+
+$SIG{INT} = \&on_sigint;
+
+while (my $line = <STDIN>)
+{
+    chop $line;
+    if ($state == WAITING_HEADER)
+    {
+        # 09:52:15.066894 TX BEACON 2bd0 (9 nodes)
+        # 09:52:13.546386 RX BEACON 2bce (9 nodes)
+        if ($line =~ /(\d\d):(\d\d):(\d\d)\.(\d+) ([TR]X) BEACON (....) \((\d+) nodes\) \(gps\[(\d+)\] ([.0-9]+) ([.0-9]+) .. ([.0-9]+) ([.0-9]+)\)/)
+        {
+            my ($hh, $mm, $ss, $frac, $dir, $addr, $numNeighbourNodes, $numsat, $latitude, $longitude, $latint, $longint) = ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12);
+            $addrTo = $addr;
+            my $validFlag = $longint ? "valid" : "invalid";
+            print "header $hh:$mm:$ss.$frac $dir $addr ($numNeighbourNodes) gps[$numsat] $latitude $longitude ($validFlag)\n";
+
+            if ($validFlag eq "valid")
+            {
+                if (int($longitude) != 18 || int($latitude) != 59)
+                {
+                    die "header $hh:$mm:$ss.$frac $dir $addr ($numNeighbourNodes) $latitude $longitude $latint $longint ($validFlag)\n";
+                }
+                $gpsCache{$addrTo} ||= {latitude => 0, longitude => 0, gpsCount => 0, longitudeLast => 0, latitudeLast => 0, numsat => 0};
+                my $obj = $gpsCache{$addrTo};
+                $obj->{longitude} += $longitude;
+                $obj->{latitude}  += $latitude;
+                $obj->{longitudeLast} = $longitude;
+                $obj->{latitudeLast}  = $latitude;
+                $obj->{numsat}        = $numsat;
+                $obj->{gpsCount}  += 1;
+            }
+
+            if ($numNeighbourNodes != NUM_NODES - 1)
+            {
+                print STDERR "ERROR: numNeighbourNodes=$numNeighbourNodes, ignoring data\n";
+                next;
+            }
+
+            if ($dir eq "TX")
+            {
+                $state = EXPECTING_TX_NODE;
+                #print "switching state to TX ($state)\n";
+                $cnt = 0;
+                %strengths = ();
+            }
+            elsif ($dir eq "RX")
+            {
+                $state = EXPECTING_RX_NODE;
+                #print "switching state to RX ($state)\n";
+                $cnt = 0;
+                %strengths = ();
+            }
+            else
+            {
+                print STDERR "ERROR: dir=$dir\n";
+            }
+        }
+    }
+    elsif ($state == EXPECTING_RX_NODE || $state == EXPECTING_TX_NODE)
+    {
+        my ($hh, $mm, $ss, $frac, $addr, $strength, $elapsed);
+        if ($state == EXPECTING_TX_NODE)
+        {
+            # 09:52:15.067535 >>> 2bce: -25.469999 dBm elapsed: 1.515000 s
+            if ($line =~ /^(\d\d):(\d\d):(\d\d)\.(\d+) >>> (....): (-?\d+\.\d+) dBm elapsed: (\d+\.\d+) s$/)
+            {
+                ($hh,$mm,$ss,$frac,$addr,$strength,$elapsed) = ($1,$2,$3,$4,$5,$6,$7);
+                #print "txnode $hh:$mm:$ss.$frac $addr $strength ($elapsed)\n";
+            }
+            else
+            {
+                print "ERROR: tx node regex did not match content: '$line'\n";
+                $state = WAITING_HEADER;
+            }
+        }
+        else
+        {
+            # 09:52:13.546997     2bd0: -24.969999 dBm
+            if ($line =~ /^(\d\d):(\d\d):(\d\d)\.(\d+)     (....): (-?\d+\.\d+) dBm$/)
+            {
+                ($hh,$mm,$ss,$frac,$addr,$strength,$elapsed) = ($1,$2,$3,$4,$5,$6,0);
+                #print "rxnode $hh:$mm:$ss.$frac $addr $strength\n";
+            }
+            else
+            {
+                print "ERROR: rx node regex did not match content: '$line'\n";
+                $state = WAITING_HEADER;
+            }
+        }
+
+        $strengths{$addr} = $strength;
+        if (++$cnt == NUM_NODES - 1)
+        {
+            $state = WAITING_HEADER;
+            on_data ();
+        }
+    }
+    elsif ($state == EXPECTING_TX_NODE)
+    {
+    }
+    else
+    {
+        print "unknown state $state\n";
+    }
+}
+
+my %maxAbsDiffs;
+my $numPackets = 0;
+sub on_data
+{
+    $numPackets++;
+    $maxAbsDiffs{$addrTo} = 0;
+    #print "on_data $addrTo\n";
+    for my $addrFrom (keys %strengths)
+    {
+        my $strength = $strengths{$addrFrom};
+        #print "$addrTo <- $addrFrom: $strength\n";
+        update_strength ($addrFrom, $addrTo, $strength);
+    }
+    print_strengths (0);
+}
+
+my %cache;
+my $lastTo = "";
+sub update_strength
+{
+    my ($from,$to,$strength,$lat,$long) = @_;
+    my $key = "$from:$to";
+    $cache{$key} ||= {strength => 0, count => 0};
+    my $obj = $cache{$key};
+
+    my $oldStrength = $obj->{strength};
+    my $oldCount    = $obj->{count};
+
+    $obj->{strength}  += $strength;
+    $obj->{count}     += 1;
+
+    if ($oldCount)
+    {
+        my $new = $obj->{strength} / $obj->{count};
+        my $old = $oldStrength / $oldCount;
+
+        my $absDiff = abs ($new - $old);
+        if ($maxAbsDiffs{$to} < $absDiff)
+        {
+            $maxAbsDiffs{$to} = $absDiff;
+        }
+    }
+    else
+    {
+        $maxAbsDiffs{$to} = 1000;
+    }
+    $lastTo = $to;
+}
+
+sub print_strengths
+{
+    my ($saveToFile) = @_;
+    my $minCnt = 10000000;
+    my $maxCnt = 0;
+    my %m;
+    for my $key (keys %cache)
+    {
+        my ($from,$to) = split (/:/, $key);
+        $m{$from} = 1;
+        $m{$to} = 1;
+    }
+    my @nodes = sort keys %m;
+
+    print "      ";
+    for my $from (@nodes)
+    {
+        print "____${from}____ ";
+    }
+    print "\n";
+
+    my $fh;
+    if ($saveToFile) {
+        open ($fh, ">log.strengths") or die;
+    }
+    for my $to (@nodes)
+    {
+
+        print "$to: ";
+        for my $from (@nodes)
+        {
+            my $key = "$from:$to";
+            my $obj = $cache{$key};
+            my $strength = ($obj && $obj->{count}) ? sprintf ("%+7.3f", $obj->{strength} / $obj->{count}) : "-------";
+            my $cnt = sprintf ("%2d", ($obj && $obj->{count}) || 0);
+            #print "$from -> $to: $strength ($cnt)\n";
+            print "$strength ($cnt) ";
+            if ($minCnt > $cnt && $from ne $to) {
+                $minCnt = $cnt + 0;
+            }
+            if ($maxCnt < $cnt && $from ne $to) {
+                $maxCnt = $cnt + 0;
+            }
+            if ($saveToFile) {
+                my $gpsDist = get_node_dist ($to, $from);
+                print $fh "$to $from $strength $gpsDist\n";
+            }
+        }
+        #print " <--- last" if ($to eq $lastTo);
+
+        my $gobj = $gpsCache{$to};
+        my $latAvg  = $gobj->{gpsCount} ? $gobj->{latitude}  / $gobj->{gpsCount} : 0;
+        my $longAvg = $gobj->{gpsCount} ? $gobj->{longitude} / $gobj->{gpsCount} : 0;
+        print sprintf (" %0.16f %0.16f (%d)\n", $latAvg, $longAvg, $gobj->{gpsCount});
+
+        if ($dumpGps && $gobj->{gpsCount})
+        {
+            my $name = $lookup{$to};
+            my $clat = 0;
+            my $clong = 0;
+            my $c = 0;
+            for my $node (@nodes)
+            {
+                my $nodeObj = $gpsCache{$node};
+                next unless $nodeObj;
+                next unless $nodeObj->{gpsCount};
+                my $latAvg  = $nodeObj->{latitude}  / $nodeObj->{gpsCount};
+                my $longAvg = $nodeObj->{longitude} / $nodeObj->{gpsCount};
+                $clat  += $latAvg;
+                $clong += $longAvg;
+                $c++;
+            }
+            $clat /= $c;
+            $clong /= $c;
+            print $gpsFh sprintf ("{\"id\":\"$to\", \"name\":\"$name\", \"lat\":%0.16f, \"long\":%0.16f, \"count\":%d,\"clat\":%0.16f,\"clong\":%0.16f, \"numsat\":%d}\n", $latAvg, $longAvg, $gobj->{gpsCount},$clat,$clong,$gobj->{numsat});
+            $gpsFh->flush ();
+        }
+    }
+    if ($saveToFile) {
+        close ($fh);
+    }
+
+    my $m = 0;
+    for my $mDiff (values %maxAbsDiffs)
+    {
+        $m = $mDiff if ($m < $mDiff);
+    }
+
+    print "maxAbsDiff:    $m\n";
+    print "minRowPackets: $minCnt\n";
+    print "maxRowPackets: $maxCnt\n";
+    print "numPackets:    $numPackets\n";
+}
+
+sub tan
+{
+    return sin ($_[0]) / cos ($_[0]);
+}
+
+sub wgs84_polar_to_cartesian
+{
+    my ($lat, $long) = @_;
+    my $a = 6378137.0;      # equatorial, semi major axis
+    my $b = 6356752.314245; # polar, semi minor axis
+
+    # world is shaped like if someone was pressing on the poles
+    # (due to more centrifugal force around the equator),
+    # resulting in a smaller polar axis than equatorial.
+    # First, use the equation for an ellipse:
+    # x^2/a^2 + z^2/b^2 = 1
+    #
+    # and let theta be the angle in the cartesian coordinate system, tan(theta) = z/x
+    #
+    # insert expression for z in the elliptic equation and get x as a function of theta:
+    # x = ab / sqrt (b^2 + a^2 * tan^2 (theta)).
+    # z = b/a  sqrt (a^2 - x^2)
+    #
+    # Now apply the longitude by rotating the vector (x,0) in the xy-plane. Then you get the vector x * (cos(phi), sin(phi)).
+    # x is now pointing out from the center of the globe toward the equator at same line as united kingdom.
+    # z is now pointing up towards the north pole
+    # y is making up a right handed coordinate system so it is pointing towards Asia.
+    # For stockholm
+    #    x > 0 (close to 0.5*$a)
+    #    y > 0 (close to 0 compared to $a)
+    #    z > 0 (close to 0.86 * $b)
+    #
+    # x 3078864.15654508
+    # y 970880.297827299
+    # z 5500783.44816461
+
+    # Let the intermediate x-coordinate we get (before rotating in the xy-plane) be called t.
+
+    my $theta = $lat / 360 * 2 * 3.141592653589;
+    my $phi   = $long / 360 * 2 * 3.141592653589;
+    my $t = $a*$b / sqrt ($b*$b + ($a*tan ($theta)) ** 2);
+
+    my $x = $t * cos ($phi);
+    my $y = $t * sin ($phi);
+    my $z = $b / $b * sqrt ($a*$a - $t * $t);
+
+    return ($x, $y, $z);
+}
+
+sub get_node_dist
+{
+    my ($node0, $node1) = @_;
+    my $obj0 = $gpsCache{$node0};
+    my $obj1 = $gpsCache{$node1};
+    if (!$obj0 || !$obj1 || !$obj0->{gpsCount} || !$obj1->{gpsCount})
+    {
+        print STDERR "insufficient gps inormation $node0 <-> $node1\n";
+        return 0;
+    }
+
+    my $lat0Avg  = $obj0->{gpsCount} ? $obj0->{latitude}  / $obj0->{gpsCount} : 0;
+    my $long0Avg = $obj0->{gpsCount} ? $obj0->{longitude} / $obj0->{gpsCount} : 0;
+    my $lat1Avg  = $obj1->{gpsCount} ? $obj1->{latitude}  / $obj1->{gpsCount} : 0;
+    my $long1Avg = $obj1->{gpsCount} ? $obj1->{longitude} / $obj1->{gpsCount} : 0;
+
+    my $dist = get_dist ($lat0Avg, $long0Avg, $lat1Avg, $long1Avg);
+    return $dist;
+}
+
+sub get_dist
+{
+    my ($lat0, $long0, $lat1, $long1) = @_;
+    my ($x0, $y0, $z0) = wgs84_polar_to_cartesian ($lat0, $long0);
+    my ($x1, $y1, $z1) = wgs84_polar_to_cartesian ($lat1, $long1);
+
+    my $dist = sqrt (($x0-$x1)**2 + ($y0-$y1)**2 + ($z0-$z1)**2);
+    return $dist;
+}
---
+++ static.pl
@@ -0,0 +1,174 @@
+#!/usr/bin/perl
+use strict;
+use JSON;
+use Math::Trig;
+
+# what we are going to do below is the following
+# * use a spherical model of the earth (e.g. not wgs84)
+# * choose three reference points from which gps information will be used
+# * first consider gps-coordinates in euclidean 3d-space
+# * execute the mds algorithm and get back coordinates in a 2d-space
+# * define a center point to be the average value of the reference points
+# * now relative to the center point in both gps-space and mds-space,
+#   we have this matrix equation that will map relative mds points to
+#   relative gps-points:
+#   / u \   / a11 a12 \ / x \
+#   | v | = | a21 a22 | |   |
+#   \ w /   \ a31 a32 / \ y /
+#   where u,v,w are gps-coordinates and x,y are mds-coordinates. The system
+#   has six unknowns which can be solved by inserting two reference points
+#   into the equation.
+# * By using the mapping above, all mds-points can now be mapped to xyz-
+#   coordinates in gps-space.
+# * transform gps-xyz-space to longitude/latitude.
+# * output estimated gps points for all nodes.
+
+
+$| = 1;
+
+my %lookup =
+(
+    "2bc6" => "andersson",
+    "2bc5" => "brolin",
+    "2bcb" => "dahlin",
+    "2bd0" => "hamrin",
+    "2bc8" => "larsson",
+    "2bc9" => "ljungberg",
+    "2bc7" => "mellberg",
+    "2bcf" => "ravelli",
+    "2bce" => "skoglund",
+    "2bca" => "zlatan"
+);
+
+my $mdsFile = "/Users/manne/field_test2/data.expansion.mds";
+my $gpsFile = "/Users/manne/field_test2/data.expansion.gps";
+
+# sample mds file:
+# 2bc5 0.000000 0.000000
+# 2bc6 9.633797 -0.000004
+# 2bc7 11.322654 0.002889
+# 2bc8 12.343378 -0.017016
+# 2bc9 10.754396 -0.014748
+# 2bca 12.494884 0.026808
+# 2bce 11.999769 -0.006333
+# 2bcf 11.768997 -0.025015
+# 2bd0 24.735229 -0.128287
+
+# sample lines from gps file:
+# {"id":"2bca", "name":"zlatan", "lat":59.3785118999999995, "long":18.0349164000000002, "count":10,"clat":59.3783008375673091,"clong":18.0352458215426878, "numsat":2}
+# {"id":"2bce", "name":"skoglund", "lat":59.3782786315789650, "long":18.0351416842105259, "count":19,"clat":59.3783008375673091,"clong":18.0352458215426878, "numsat":4}
+# {"id":"2bcf", "name":"ravelli", "lat":59.3783939999999930, "long":18.0354202500000014, "count":12,"clat":59.3783008375673091,"clong":18.0352458215426878, "numsat":1}
+# {"id":"2bd0", "name":"hamrin", "lat":59.3785048000000089, "long":18.0353416000000024, "count":10,"clat":59.3783008375673091,"clong":18.0352458215426878, "numsat":3}
+
+my @refNodes = ("2bc5", "2bc7", "2bd0");
+
+sub read_gps_ref_points
+{
+    my ($gpsFile, @refNodes) = @_;
+    open (my $fh, "<$gpsFile") or die "open $gpsFile: $!";
+    my %db;
+    while (my $line = <$fh>)
+    {
+        my $obj = from_json ($line);
+        $db{$obj->{id}} = $obj;
+    }
+    close ($fh);
+
+    my $gpsRefPoints = {};
+    for (@refNodes)
+    {
+        die "ref node not found: $_" unless (exists $db{$_});
+        $gpsRefPoints->{$_} = {latitude => $db{$_}->{lat}, longitude => $db{$_}->{long}};
+    }
+    return $gpsRefPoints;
+}
+
+sub read_mds_points
+{
+    my ($mdsFile, @refNodes) = @_;
+    open (my $fh, "<$mdsFile") or die "open $mdsFile: $!";
+    my %db;
+    my $mdsPoints = {};
+    while (my $line = <$fh>)
+    {
+        chop $line;
+        my ($id, $x, $y) = split (/ /, $line);
+        $mdsPoints->{$id} = {x => $x+0, y => $y+0};
+
+    }
+    close ($fh);
+    return $mdsPoints;
+}
+
+my $gpsRefPoints = read_gps_ref_points ($gpsFile, @refNodes);
+my $mdsPoints = read_mds_points ($mdsFile);
+
+my $pi = 3.1415926535897932384626433832795;
+my $nRefs = scalar @refNodes;
+my ($mdsRefCenterX, $mdsRefCenterY) = (0,0);
+my ($gpsRefCenterX, $gpsRefCenterY, $gpsRefCenterZ) = (0,0,0);
+
+for my $node (@refNodes)
+{
+    my $latitude  = $gpsRefPoints->{$node}->{latitude};
+    my $longitude = $gpsRefPoints->{$node}->{longitude};
+    my $r     = 6371e3;
+    my $theta = (90 - $latitude) / 180 * $pi;
+    my $phi   = $longitude / 180 * $pi;
+
+    $gpsRefPoints->{$node}->{x} = $r * sin ($theta) * cos ($phi);
+    $gpsRefPoints->{$node}->{y} = $r * sin ($theta) * sin ($phi);
+    $gpsRefPoints->{$node}->{z} = $r * cos ($theta);
+
+    $mdsRefCenterX += $mdsPoints->{$node}->{x} / $nRefs;
+    $mdsRefCenterY += $mdsPoints->{$node}->{y} / $nRefs;
+    $gpsRefCenterX += $gpsRefPoints->{$node}->{x} / $nRefs;
+    $gpsRefCenterY += $gpsRefPoints->{$node}->{y} / $nRefs;
+    $gpsRefCenterZ += $gpsRefPoints->{$node}->{z} / $nRefs;
+}
+#print "$gpsRefCenterX $gpsRefCenterY $gpsRefCenterZ\n";
+
+my $x0 = $mdsPoints->{$refNodes[0]}->{x} - $mdsRefCenterX;
+my $y0 = $mdsPoints->{$refNodes[0]}->{y} - $mdsRefCenterY;
+
+my $x1 = $mdsPoints->{$refNodes[1]}->{x} - $mdsRefCenterX;
+my $y1 = $mdsPoints->{$refNodes[1]}->{y} - $mdsRefCenterY;
+
+my $u0 = $gpsRefPoints->{$refNodes[0]}->{x} - $gpsRefCenterX;
+my $v0 = $gpsRefPoints->{$refNodes[0]}->{y} - $gpsRefCenterY;
+my $w0 = $gpsRefPoints->{$refNodes[0]}->{z} - $gpsRefCenterZ;
+
+my $u1 = $gpsRefPoints->{$refNodes[1]}->{x} - $gpsRefCenterX;
+my $v1 = $gpsRefPoints->{$refNodes[1]}->{y} - $gpsRefCenterY;
+my $w1 = $gpsRefPoints->{$refNodes[1]}->{z} - $gpsRefCenterZ;
+
+my $a12 = ($x1 * $u0 - $x0 * $u1) / ($x1 * $y0 - $x0 * $y1);
+my $a22 = ($x1 * $v0 - $x0 * $v1) / ($x1 * $y0 - $x0 * $y1);
+my $a32 = ($x1 * $w0 - $x0 * $w1) / ($x1 * $y0 - $x0 * $y1);
+
+my $a11 = ($u0 - $a12 * $y0) / $x0;
+my $a21 = ($v0 - $a22 * $y0) / $x0;
+my $a31 = ($w0 - $a32 * $y0) / $x0;
+
+my $gpsPoints = {};
+for my $node (keys %$mdsPoints)
+{
+    my $mdsX = $mdsPoints->{$node}->{x} - $mdsRefCenterX;
+    my $mdsY = $mdsPoints->{$node}->{y} - $mdsRefCenterY;
+
+    my $gpsX = $a11 * $mdsX + $a12 * $mdsY + $gpsRefCenterX;
+    my $gpsY = $a21 * $mdsX + $a22 * $mdsY + $gpsRefCenterY;
+    my $gpsZ = $a31 * $mdsX + $a32 * $mdsY + $gpsRefCenterZ;
+
+    my $r = sqrt ($gpsX**2 + $gpsY**2 + $gpsZ**2);
+    my $thetaRad = $pi / 2 - acos ($gpsZ / $r);
+    my $phiRad   = atan ($gpsY / $gpsX);
+
+    my $lat  = $thetaRad / $pi * 180;
+    my $long = $phiRad   / $pi * 180;
+    $gpsPoints->{$node}->{latitude}  = $lat;
+    $gpsPoints->{$node}->{longitude} = $long;
+
+    print "$node $lat $long\n";
+}
+
